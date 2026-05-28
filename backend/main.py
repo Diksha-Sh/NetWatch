da365671a4f2d5c75c7fe7b171cf2a05498821b5
Diksha-Sh <dikshasharma5014@gmail.com>
2026-05-28 22:06:49 +0530

    update

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+
 app = FastAPI()
+
 @app.get("/")
 def root():
-    return {"status":"NET-WATCH ALIVE"}
+    return {"status": "NET-WATCH alive"}
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    contents = await file.read()  # reads the raw bytes of the file
+    return {
+        "filename": file.filename,
+        "size": len(contents),
+        "content_type": file.content_type
+    }
