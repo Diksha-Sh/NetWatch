116a7bef3af25e04ec61d57c2bf84c5e9c8c4c7e
Diksha-Sh <dikshasharma5014@gmail.com>
2026-05-31 23:44:51 +0530

    feat: PCAP file upload + Scapy parser working

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,34 @@
+#import necessary packages
 from fastapi import FastAPI, UploadFile, File
+from scapy.all import rdpcap
+import tempfile
+import os
 
 app = FastAPI()
-
 @app.get("/")
 def root():
-    return {"status": "NET-WATCH alive"}
-
+    return {"status": "NetWatch alive"}
 @app.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
-    contents = await file.read()  # reads the raw bytes of the file
+async def upload_pcap(file: UploadFile = File(...)):
+    # Save uploaded file temporarily
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pcap") as tmp:
+        contents = await file.read()
+        tmp.write(contents)
+        tmp_path = tmp.name
+
+    # Read with Scapy(the pcap files)
+    packets = rdpcap(tmp_path)
+
+    #Delete temp file
+    os.remove(tmp_path)
+
+    # Extract basic info from first 5 packets
+    packet_info = []
+    for pkt in packets[:5]:
+        packet_info.append(str(pkt.summary()))
+
     return {
         "filename": file.filename,
-        "size": len(contents),
-        "content_type": file.content_type
-    }
+        "total_packets": len(packets),
+        "first_5_packets": packet_info
+    }a
